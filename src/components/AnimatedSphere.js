f9ce47dc039d76d92da489244837d5a4ea2614ce
adamochi <honeyrumballs@gmail.com>
2022-06-19 21:22:40 +1000

    three

--- src/components/AnimatedSphere.js
+++ src/components/AnimatedSphere.js
@@ -3,11 +3,11 @@ import { Sphere, MeshDistortMaterial } from "@react-three/drei/core";
 
 const AnimatedSphere = () => {
   return (
-    <Sphere visible args={[1.5, 100, 200]} scale={1.6}>
+    <Sphere visible args={[1, 100, 200]} scale={2}>
       <MeshDistortMaterial
         color="#8352FD"
         attach="material"
-        distort={0.3}
+        distort={1}
         speed={2.5}
       />
     </Sphere>
--- src/components/Boxtwo.js
+++ src/components/Boxtwo.js
@@ -1,9 +1,5 @@
 import React from "react";
 
-import { useLoader } from "@react-three/fiber";
-import { TextureLoader } from "three";
-import texture from "../assets/images/mossyrock.jpg";
-
 const Boxtwo = () => {
   return (
     <mesh rotation={[90, 0, 20]}>
