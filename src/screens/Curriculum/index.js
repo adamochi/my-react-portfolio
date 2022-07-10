09c6c062decf13617465be6ce5a15d78dae9da17
adamochi <honeyrumballs@gmail.com>
2022-07-10 22:41:11 +1000

    courses pager

--- src/screens/Curriculum/index.js
+++ src/screens/Curriculum/index.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./index.scss";
+import AnimatedLetters from "../../components/AnimatedLetters";
 
 const Curriculum = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 4500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
   return (
-    <div className="Curriculum">
-      <div></div>
-    </div>
+    <>
+      <header className="page-title">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={"Courses & Components Covered".split("")}
+            idx={15}
+          />
+        </h1>
+      </header>
+      <div className="curriculum-page container"></div>
+    </>
   );
 };
 
